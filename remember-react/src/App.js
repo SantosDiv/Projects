be7dc4a4dd365b499acc0014074d2ffa6df09d66
Diogenes <diogenesfelipe.s@outlook.com>
2021-03-13 16:55:24 -0300

    remember-react

--- remember-react/src/App.js
+++ remember-react/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 class App extends React.Component {
   render() {
     return (
-     <p>principal</p>
+     <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/dashboard/:user" render={(props) => <Dashboard {...props} />}/>
+      </Switch>
+     </BrowserRouter>
     );
   }
 }
