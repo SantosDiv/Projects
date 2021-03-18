c8138ee63acdf6ce4ff97bd661005a13de3c7e3d
Diogenes <diogenesfelipe.s@outlook.com>
2021-03-18 11:47:06 -0300

    feat/Criaçao da do jsx do acordeon para os capitulos dos cursos

--- study-plataform/src/components/mainContent/CourseItem.js
+++ study-plataform/src/components/mainContent/CourseItem.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ChapterLesson from './ChapterLesson';
 import '../../css/CourseItem.css';
 
 class CourseItem extends React.Component {
   render() {
     const { course } = this.props;
-    const { name, module } = course;
+    const { name, module, contents } = course;
     return(
       <Link to={`/modulo${module}/${name}`} >
         <section className="course">
           <p>{ name }</p>
           <i className="fas fa-chevron-down"></i>
         </section>
+        <ul className="contents">
+          { contents.map((chapter) => <ChapterLesson chapter={ chapter } nameCourse={ name } />)}
+        </ul>
       </Link>
     );
   }
